42e05268a70c801b13a22003c42de072015d9e15
Rich Lewis <2941720+lewisacidic@users.noreply.github.com>
2019-09-11 13:57:10 +0100

    test: adapt tests for new about implementation

--- {{ cookiecutter.project_name }}/tests/test_about.py
+++ {{ cookiecutter.project_name }}/tests/test_about.py
@@ -1,21 +1,35 @@
 {{ cookiecutter.header }}
 """Tests for metadata loading."""
 
-import pytest
+import pathlib
 import importlib
-try:
-    import importlib.metadata as importlib_metadata
-except ModuleNotFoundError:
-    import importlib_metadata
+import runpy
+
+import pytest
 
 
-root = importlib.import_module("{{ cookiecutter.project_slug }}")
+@pytest.fixture
+def base_pkg():
+    """Provide a reloaded base package as a fixture."""
+    base_pkg = importlib.import_module("{{ cookiecutter.project_slug }}")
+    return importlib.reload(base_pkg)
 
 
-@pytest.parametrize("field", ["distname", "name", "copyright", "license", "name", "url", "version"])
-def test_metadata(field):
-    """Test metadata is available on base package"""
-    importlib.reload(root)
-    assert getattr(root, f"__{field}__") is not None
+@pytest.mark.parametrize(["field", "value"], [
+    ("distname", "{{ cookiecutter.project_name }}"),
+    ("name", "{{ cookiecutter.project_slug }}"),
+    ("copyright", "{{ cookiecutter.copyright }}"),
+    ("license", "{{ cookiecutter.license }}"),
+    ("url", "{{ cookiecutter.project_url }}"),
+])
+def test_metadata(base_pkg, field, value):
+    """Test metadata is available on base package."""
+    assert getattr(base_pkg, f"__{field}__") is not None
 
 
+def test_version(base_pkg):
+    """Test the version is correctly detected with versioneer."""
+    versioneer_path = pathlib.Path(__file__).parents[1].joinpath("versioneer.py")
+    versioneer = runpy.run_path(versioneer_path)
+    version = versioneer["get_version"]()
+    assert base_pkg.__version__ == version
