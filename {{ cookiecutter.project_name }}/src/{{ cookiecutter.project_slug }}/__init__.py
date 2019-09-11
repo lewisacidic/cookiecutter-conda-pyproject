6239e6a9bd76b64d9d9f4fc45b4e8d6ca7c3645f
Rich Lewis <2941720+lewisacidic@users.noreply.github.com>
2019-09-11 11:59:21 +0100

    refactor: remove some metadata from base package
    The __about__ has more information.
    
    Signed-off-by: Rich Lewis <2941720+lewisacidic@users.noreply.github.com>

--- {{ cookiecutter.project_name }}/src/{{ cookiecutter.project_slug }}/__init__.py
+++ {{ cookiecutter.project_name }}/src/{{ cookiecutter.project_slug }}/__init__.py
@@ -1,24 +1,19 @@
 {{ cookiecutter.header }}
-"""{{ cookiecutter.project_description }}"""
+"""{{ cookiecutter.project_description }}
 
-from .__about__ import __author__
-from .__about__ import __author_email__
+See `{{ cookiecutter.project_slug }}.__about__` for more info.
+"""
+
 from .__about__ import __copyright__
-from .__about__ import __description__
 from .__about__ import __distname__
 from .__about__ import __license__
-from .__about__ import __name__
 from .__about__ import __url__
 from .__about__ import __version__
 
 __all__ = [
-    "__author__",
-    "__author_email__",
     "__copyright__",
-    "__description__",
     "__distname__",
     "__license__",
-    "__name__",
     "__url__",
     "__version__"
 ]
