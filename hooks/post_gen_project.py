9ad9df3d84269a7ede934d6baca92b2e89231d13
Rich Lewis <2941720+lewisacidic@users.noreply.github.com>
2020-02-10 14:57:59 +0000

    refactor: move licenses into separate templates

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -9,12 +9,20 @@ from subprocess import DEVNULL
 if __name__ == "__main__":
     proj = pathlib.Path.cwd()
 
+    # create the .gitattributes (required for versioneer) now
     attrpath = proj.joinpath(".gitattributes")
     attrpath.write_text("src/{{ cookiecutter.project_slug }}/_version.py export-subst")
 
-    if "{{ cookiecutter.license }}" == "No license":
-        proj.joinpath("LICENSE").unlink()
+    # dont include a license if no license provided
+    if "{{ cookiecutter.license }}" != "No license":
+        shutil.move(
+            proj.joinpath("licenses", "{{ cookiecutter.license }}"),
+            proj.joinpath("LICENSE"),
+        )
 
+    shutil.rmtree(proj.joinpath("licenses"))
+
+    # remove config for other vcs type
     if "{{ cookiecutter.vcs_remote_type}}" == "github":
         shutil.rmtree(proj.joinpath(".gitlab"))
     elif "{{ cookiecutter.vcs_remote_type}}" == "gitlab":
@@ -25,7 +33,7 @@ if __name__ == "__main__":
     # add pre-commit hooks
     for hook in "pre-commit", "commit-msg":
         subprocess.call(["pre-commit", "install", "--hook-type", hook])
-    
+
     # run the precommit hooks to fix anything broken
     subprocess.call(["git", "add", "*"])
     subprocess.call(["pre-commit", "run", "--all"], stdout=DEVNULL)
@@ -38,7 +46,7 @@ if __name__ == "__main__":
     to scaffold the project.
     """
 
-    if subprocess.call(["git", "commit", f'-m{msg}']):
+    if subprocess.call(["git", "commit", f"-m{msg}"]):
         print("Failed to create initial commit.")
     if subprocess.call(["git", "tag", "v0.0.0", "-m", "0.0.0: initial release"]):
         print("Failed to create tag.")
