2e17b2b5d6ddc4567a8b11f6eaed3b42787562a4
Rich Lewis <2941720+lewisacidic@users.noreply.github.com>
2020-02-20 00:12:32 +0000

    fix: add tag prefix to versioneer config

--- {{ cookiecutter.project_name }}/src/{{ cookiecutter.project_slug }}/_version.py
+++ {{ cookiecutter.project_name }}/src/{{ cookiecutter.project_slug }}/_version.py
@@ -37,7 +37,7 @@ def get_config():
     cfg = VersioneerConfig()
     cfg.VCS = "git"
     cfg.style = "pep440"
-    cfg.tag_prefix = ""
+    cfg.tag_prefix = "v"
     cfg.parentdir_prefix = "{{ cookiecutter.project_name }}-"
     cfg.versionfile_source = "src/{{ cookiecutter.project_slug }}/_version.py"
     cfg.verbose = False
